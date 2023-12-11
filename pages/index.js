11c029098cc9567246f226e5298aadce103d62e5
Tatiany <153239272+devtatiany@users.noreply.github.com>
2023-12-11 01:23:14 +0000

    enviando os demais arquivos e pastas

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home(){
+    return <h1>Que dia é hoje? Responde lá no zap por favor!</h1>
+}
+
+export default Home; 
+//comando para reinderizar a funcao Home
+//e definir ela como a default (principal - primeira)
+
+
