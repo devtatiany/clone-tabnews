3b0beefa50118bd4ad80b2770935d098e7fa8eaa
Tatiany <153239272+devtatiany@users.noreply.github.com>
2023-12-15 01:36:57 +0000

    enviando arquivos corrigidos pelo prettier

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
-function Home(){
-    return <h1>Que dia é hoje? Responde lá no zap por favor!</h1>
+function Home() {
+  return <h1>Que dia é hoje? Responde lá no zap por favor!</h1>;
 }
 
-export default Home; 
+export default Home;
 //comando para reinderizar a funcao Home
 //e definir ela como a default (principal - primeira)
-
-
